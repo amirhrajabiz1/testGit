7c876b5e3bdf3f898b07d1ffa0236e3b22cf902a
Amir Rajabi <amirhrajabiz1@gmail.com>
2021-11-06 13:26:40 +0330

    add score

--- guess-number-game.py
+++ guess-number-game.py
@@ -9,8 +9,10 @@ b = int(input("enter the second number of range: "))
 n = random.randint(a, b)
 print("I choose a number between", a, "to", b)
 
+step = 0
 while(True):
     g = int(input("guess: "))
+    step += 1
     if(g == n):
         print("you win")
         break
@@ -20,6 +22,8 @@ while(True):
         print("enter lower number")
 end_time = time.time()
 
-print("your game takes %.2f second(s)" %(end_time - start_time))
+t = end_time - start_time
+print("your game takes %.2f second(s)" %(t))
 
-
+score = (b * 2) / (t * 3 + step * 4)
+print("your score is: %.2f" %score)
